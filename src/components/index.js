4b53775faea5fe5daf759739e1e7a9825984b491
Reborn233 <499289113@qq.com>
2021-11-05 11:52:10 +0800

    fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import appTable from './app-table/index';
+import appFilter from './app-filter/index';
 import appForm from './app-form/index';
 import appPanel from './app-panel/index';
-import topFilter from './top-filter/index';
 import fileButton from './file-button/index';
 import appSection from './app-section';
 
 Vue.component('app-table', appTable);
 Vue.component('app-form', appForm);
-Vue.component('top-filter', topFilter);
 Vue.component('file-button', fileButton);
 Vue.component('app-section', appSection);
 Vue.component('app-panel', appPanel);
+Vue.component('app-filter', appFilter);
