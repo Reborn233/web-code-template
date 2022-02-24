0c6cc20fdc199a12ef0d8bcd49814d62a4a127b9
Reborn233 <499289113@qq.com>
2022-02-24 10:14:42 +0800

    更新scss全局变量自动引入

--- vue.config.js
+++ vue.config.js
@@ -53,9 +53,10 @@ module.exports = {
     loaderOptions: {
       // 给 sass-loader 传递选项
       sass: {
-        // @/ 是 src/ 的别名
-        // 所以这里假设你有 `src/variables.scss` 这个文件
-        // data: '@import "@styles/index.scss";'
+        additionalData: `@import "@/styles/variables.scss"`
+      },
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`
       }
     }
   },
